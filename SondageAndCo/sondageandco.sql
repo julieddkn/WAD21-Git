7f64e4e3e7557464752c7957d8413948e9ce8f4c
jdntk <jdntk@live.be>
2021-11-29 12:38:37 +0100

    modified db

--- SondageAndCo/sondageandco.sql
+++ SondageAndCo/sondageandco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 29 nov. 2021 à 12:02
+-- Généré le : lun. 29 nov. 2021 à 12:20
 -- Version du serveur : 10.4.21-MariaDB
 -- Version de PHP : 8.0.10
 
@@ -24,18 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `choixmultiples`
---
-
-CREATE TABLE `choixmultiples` (
-  `id` int(11) NOT NULL,
-  `proposedanswers` varchar(500) NOT NULL,
-  `idQuestions` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `entreprise`
 --
 
@@ -46,28 +34,9 @@ CREATE TABLE `entreprise` (
   `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `sondage`
---
-
-CREATE TABLE `sondage` (
-  `id` int(11) NOT NULL,
-  `intitulé` varchar(50) NOT NULL,
-  `type` tinyint(1) NOT NULL,
-  `openquestions` varchar(7000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 --
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `choixmultiples`
---
-ALTER TABLE `choixmultiples`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `entreprise`
@@ -76,31 +45,13 @@ ALTER TABLE `entreprise`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `sondage`
---
-ALTER TABLE `sondage`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
---
--- AUTO_INCREMENT pour la table `choixmultiples`
---
-ALTER TABLE `choixmultiples`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `entreprise`
 --
 ALTER TABLE `entreprise`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `sondage`
---
-ALTER TABLE `sondage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
