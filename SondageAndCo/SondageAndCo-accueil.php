1dba516954eaebdfcd7be0019881c23f6a0e9b53
CaroCap <carolinecap.event@gmail.com>
2021-11-29 12:42:17 +0100

    class Sondage

--- SondageAndCo/SondageAndCo-accueil.php
+++ SondageAndCo/SondageAndCo-accueil.php
@@ -1,10 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-// CONNEXION DB
-include_once "./config/db.php";
-        try {
-            $bdd = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME . ';charset='. DBCHARSET, DBUSER, DBPASS);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            die();
-        }   
+    // CONNEXION DB
+    include_once "./config/db.php";
+            try {
+                $bdd = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME . ';charset='. DBCHARSET, DBUSER, DBPASS);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                die();
+            }   
+    ?>
+
+    
+</body>
+</html>
---
+++ SondageAndCo/classes/Sondage.class.php
@@ -0,0 +1,103 @@
+<?php
+
+class Sondage {
+
+    public int $id;
+    public string $intitule;
+    public bool $type;
+    public string $reponse;
+
+    public function __construct(array $vals)
+    {
+        $this->hydrate($vals);
+    }
+
+    public function hydrate (array $vals){
+        foreach ($vals as $nomPropriete => $valeurPropriete) {
+            if (isset($vals[$nomPropriete])) {
+                // donner une valeur `a la proprieté
+                $this->$nomPropriete = $valeurPropriete;
+            } 
+        }
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getid()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setid($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of intitule
+     */ 
+    public function getIntitule()
+    {
+        return $this->intitule;
+    }
+
+    /**
+     * Set the value of intitule
+     *
+     * @return  self
+     */ 
+    public function setIntitule($intitule)
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of reponse
+     */ 
+    public function getReponse()
+    {
+        return $this->reponse;
+    }
+
+    /**
+     * Set the value of reponse
+     *
+     * @return  self
+     */ 
+    public function setReponse($reponse)
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+}
